c0d701a9f73f22df7f198a68af149fcac6c8d515
BashkimGrepi <bashkim.grepi@gmail.com>
2025-08-11 04:30:20 +0300

    handles login, register and profile endpoints

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Post, Body, UseGuards, Get, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Public } from 'src/decorators/public.decorator';
+import { UserResponseDto } from './dto/user-response.dto';
+
+
+@UseGuards(JwtAuthGuard)
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Public()
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Public()
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() req) {
+    return new UserResponseDto(req.user);
+  }
+}
